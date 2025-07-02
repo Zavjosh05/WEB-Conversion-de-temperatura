5042e792dff1e8ce2fa033574679e3dddedeeef2
Zavaleta Guerrero Joshua Ivan <118492689+Zavjosh05@users.noreply.github.com>
2025-07-01 18:28:27 -0600

    proyecto subido

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  titulo = 'Ejercicio Base';
+}
